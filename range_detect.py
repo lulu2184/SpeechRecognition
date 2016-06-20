239d82cb87abb03a5e83be79970870264f653810
lulu2184 <lulu2184@gmail.com>
2016-06-20 15:00:15 +0800

    speech range detection

---
+++ range_detect.py
@@ -0,0 +1,65 @@
+import math
+import numpy as np
+
+def calVolume(waveData, frameSize, step):
+	wlen = len(waveData)
+	frameNum = int(math.ceil(wlen * 1.0 / step))
+	volume = np.zeros((frameNum, 1))
+	for i in range(frameNum):
+		curFrame = waveData[np.arange(i * step, min(i * step + frameSize, wlen))]
+		curFrame = curFrame - np.mean(curFrame)
+		volume[i] = np.sum(np.abs(curFrame)) / frameSize
+	return volume
+
+def calZeros(waveData, frameSize, step):
+	wlen = len(waveData)
+	frameNum = int(math.ceil(wlen * 1.0 / step))
+	zeros = np.zeros((frameNum, 1))
+	for i in range(frameNum):
+		curFrame = waveData[np.arange(i * step, min(i * step + frameSize, wlen))]
+		curFrame = curFrame - np.mean(curFrame);
+		zeros[i] = np.sum(np.abs(np.sign(curFrame[0:-1]) - np.sign(curFrame[1::]))) / frameSize / 2
+	return zeros
+
+def calEnergy(waveData, frameSize, step):
+	wlen = len(waveData)
+	frameNum = int(math.ceil(wlen * 1.0 / step))
+	energy = np.zeros((frameNum, 1))
+	for i in range(frameNum):
+		curFrame = waveData[np.arange(i * step, min(i * step + frameSize, wlen))]
+		# curFrame = curFrame - np.median(curFrame)
+		energy[i] = np.sum(np.square(curFrame)) / frameSize
+	return energy
+
+def range_detect(waveData):
+	frameSize = 256
+	step = 64
+	volume = calVolume(waveData, frameSize, step)
+	zeros = calZeros(waveData, frameSize, step)
+
+	z0 = 0.25
+	z1 = 0.35
+	mh = 0.2
+	ml = 0.1
+	wlen = len(waveData)
+
+	mh_array = [i for (i, w) in enumerate(volume) if w > mh]
+	start = min(mh_array)
+	end = max(mh_array)
+	array = [(start * step, end * step + frameSize)]
+
+	start = min([i for (i, z) in enumerate(zeros) if z > z1] + [start])
+	end = max([i for (i, z) in enumerate(zeros) if z > z1] + [end])
+	array.append((start * step, end * step + frameSize))
+
+	start = max([i for (i, w) in enumerate(volume) if w < ml and i < start])
+	end = min([i for (i, w) in enumerate(volume) if w < ml and i > end])
+	array.append((start * step, end * step + frameSize))
+
+	start = max([i for (i, z) in enumerate(zeros) if z < z0 and i < start])
+	end = min([i for (i, z) in enumerate(zeros) if z < z0 and i > end])
+	array.append((start * step, end * step + frameSize))
+
+	start = start * step
+	end = end * step + frameSize
+	return array
---
+++ test.py
@@ -0,0 +1,69 @@
+import math
+import wave
+import sys
+import numpy as np
+import pylab as pl
+import range_detect as rd
+
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print('wrong params')
+		sys.exit()
+	fw = wave.open(sys.argv[1], 'r')
+	params = fw.getparams()
+	print(params)
+	nchannels, sampwidth, framerate, nframes = params[:4]
+	strData = fw.readframes(nframes)
+	orgData = np.fromstring(strData, dtype = np.int16)
+	waveData = orgData - np.mean(orgData);
+	waveData = waveData * 1.0 / max(abs(waveData))
+	fw.close()
+
+	frameSize = 256
+	step = 64
+	volume11 = rd.calVolume(waveData, frameSize, step)
+	volume12 = rd.calZeros(waveData, frameSize, step)
+
+	time = np.arange(0, nframes) * (1.0 / framerate)
+	time2 = np.arange(0, len(volume11)) * step * 1.0 / framerate
+
+	# (start, end) = rd.range_detect(waveData)
+	array = rd.range_detect(waveData)
+
+	pl.subplot(3, 1, 1)
+	pl.plot(time, waveData, color="black")
+	colors = ['-r', '-g', '-b', 'yellow']
+	print array
+	for (i, (start, end)) in enumerate(array):
+		start = start * 1.0 / framerate
+		end = end * 1.0 / framerate
+		pl.plot([start, start], [-1, 1], colors[i])
+		pl.plot([end, end], [-1, 1], colors[i])
+	pl.ylabel("Amplitude")
+	pl.subplot(3, 1, 2)
+	pl.plot(time2, volume11)
+	for (i, (start, end)) in enumerate(array):
+		start = start * 1.0 / framerate
+		end = end * 1.0 / framerate
+		pl.plot([start, start], [0, 0.5], colors[i])
+		pl.plot([end, end], [0, 0.5], colors[i])
+	pl.ylabel("absSum")
+	pl.subplot(3, 1, 3)
+	pl.plot(time2, volume12)
+	for (i, (start, end)) in enumerate(array):
+		start = start * 1.0 / framerate
+		end = end * 1.0 / framerate
+		pl.plot([start, start], [0, 0.5], colors[i])
+		pl.plot([end, end], [0, 0.5], colors[i])
+	pl.ylabel("ZeroCount")
+	pl.show()
+
+	start, end = array[3]
+	f = wave.open(r"out.wav", "wb")
+
+	f.setparams(params)
+	f.setnframes(end - start);
+	f.writeframes(orgData[start:end].tostring())
+
+	f.close()
